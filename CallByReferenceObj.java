0c4ef3c296192d26c905f803b56d5d773390ea38
Vaibhav-Raj10 <vaibhav.raj.engg@gmail.com>
2020-04-06 23:43:01 +0530

    06/04/2020

---
+++ CallByReferenceObj.java
@@ -0,0 +1,14 @@
+/*
+1. Two ways of passing arguments to a method:
+	a. call by value
+	b. call by reference
+2. What is a reference?
+	reference is a variable that holds an address
+3. When we pass a refernce as argumnet, we are passing the value of reference as opposed to the object itself.
+   Value of a refernce variable is an address where the object is stored.
+   Parameter then recieves this address and thus stores the same address as referece variable and this points to
+   same object as reference variable;
+*/
+
+// Homework:- WAP to pass an object by refernce to a method and swap 'i' and 'j' instance variable of the object
+// Homework:- In same program SOP the reference variable and see what is printed
