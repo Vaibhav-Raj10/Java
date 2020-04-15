08b8819d748e08793a0d97bfac37b6bf3a1c64f5
Vaibhav-Raj10 <vaibhav.raj.engg@gmail.com>
2020-04-16 00:26:21 +0530

    into the world of inheritence

---
+++ Inheritance/SuperClassRef.java
@@ -0,0 +1,13 @@
+/*
+-------------------------------------------------------------------------
+				INHERITANCE 001
+-------------------------------------------------------------------------
+
+1. A subclass inherits all of the members of it's superclass (but not private one)
+2. Superclass does NOT know about members of subclass. It is an independent, stand-alone class.
+3. A subclass can act as a superclass of another class.
+4. Superclass reference variables can refer subclass objects
+5. It is the type of reference variable that determines which members can be accessed.
+6. Subclass reference variable cannot point to super class object.
+*/
+
